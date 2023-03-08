d3fd920340f8abf7baf05ea2c738a2a445dd04ba
egasner <egasner68@gmail.com>
2023-03-07 22:01:19 -0600

    json and renamed

---
+++ Midterm/index.js
@@ -0,0 +1,39 @@
+fetch('index.json')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+
+.catch(function (err) {
+    console.log('error:' + err);
+})
+
+function appendData(data){
+    let meme1 = document.getElementById("jammingCat");
+    let meme2 = document.getElementById("kermit");
+    let meme3 = document.getElementById("dylan");
+    let poopy = 1;
+
+
+    for (let meme of data) {
+        let div = document.createElement("div");
+        console.log(meme);
+        div.innerHTML = `<h2> ${meme["title"]} </h2><p> ${meme["description"]} </p> <p> ${meme["year"]} </p>`;
+        if(poopy == 1){
+            meme1.appendChild(div);
+        }else if(poopy == 2){
+            meme2.appendChild(div);
+        }else
+            meme3.appendChild(div);
+        
+        poopy++;
+        // for (let element of data[productName]) {
+        //     console.log(element);
+        //     let div2 = document.createElement("div");
+        //     div2.innerHTML = `${element["productId"]} : ${element["shortDescription"]} <br>`;
+        //     mainContainer.appendChild(div2);
+        // }
+    }
+}
